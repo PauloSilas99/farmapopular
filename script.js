93a395e08279c9210b8176d9f7a51d01bfaf1b97
PauloSilas99 <fonsecasodreps@hotmail.com>
2023-10-04 18:15:35 -0300

    farma-2

--- script.js
+++ script.js
@@ -23,3 +23,10 @@ fundoamarelo.addEventListener('mousemove',function(e){
     }
     this.scrollLeft += startX - e.clientX
 });
+
+window.sr=ScrollReveal({ reset: true});
+sr.reveal('.float-left',{rotate:{x:100,y:20,z:50},duration:1850});
+
+sr.reveal('.direita',{rotate:{x:200,y:0,z:0},duration:2500});
+
+sr.reveal('.esquerda',{rotate:{x:100,y:20,z:50},duration:2000});
